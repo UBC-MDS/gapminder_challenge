78741231160214968fc1ffd6e030b8d3c0d661b0
irene93 <irene93@student.ubc.ca>
2022-03-03 14:37:04 -0800

    Add graph title and tooltip

--- src/gapminder_challenge/dashboard/dash_app4.py
+++ src/gapminder_challenge/dashboard/dash_app4.py
@@ -6,12 +6,19 @@ import altair as alt
 df = pd.read_csv('../../data/raw/world-data-gapminder_raw.csv')
 df = df[["country", "year", "population", "region", 'income_group', "income"]]
 
+# Define constant values
+YEAR_MIN = 1901
+YEAR_MAX = 2018
+YEAR_INTERVAL = 10
+INCOME_UNIT = 1000000
+
+
 url = '/dash_app4/'
 
 def add_dash(server):
     """
     It creates a Dash app that plots a scatter plot of income per capita by 
-        year from 1901 to 2018
+        year for the chosen countries, regions, and income groups.
     
     :param server: The Flask app object
     :return: A Dash server
@@ -22,12 +29,16 @@ def add_dash(server):
         html.Iframe(
             id='main_chart',
             style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.RangeSlider(min=1901, max=2018,
-                   value=[1901, 2018],
+            html.Label([
+                'Zoom in Years: ',
+                dcc.RangeSlider(min=1901, max=2018,
+                   value=[YEAR_MIN, YEAR_MAX],
                    id='year_slider',
                    marks={str(year): str(year)
-                          for year in range(1901, 2018, 10)}
+                          for year in range(YEAR_MIN, YEAR_MAX, YEAR_INTERVAL)}
                    )
+            ]),
+        
         #            ,
         # dcc.Dropdown(df_year.region.unique(),
         #              id='dropdown',
@@ -45,26 +56,22 @@ def add_dash(server):
     def plot_altair(year_slider):
         """
         The function
-        :param 
+        :param year_slider: The year range to plot
         :param 
         :return: The Altair chart is being returned.
         """
         df_by_year = df.groupby(["year"]).sum()
-        df_by_year["income_per_capita"] = df_by_year["income"] * 1000000 / df_by_year["population"]
+        df_by_year["income_per_capita"] = round(df_by_year["income"] * INCOME_UNIT / df_by_year["population"], 1)
         df_by_year = df_by_year.reset_index()
         chart = alt.Chart(df_by_year.query(
-            f'year>={year_slider[0]} and year<={year_slider[1]}')
-            ).mark_point(opacity=0.5
-            ).encode(alt.X('year', title='Year', 
-            scale=alt.Scale(domain=[year_slider[0], year_slider[1]])),
-                    alt.Y('income_per_capita', title='Income Per Capita'))
-        # text = chart.mark_text(
-        #     align='left',
-        #     baseline='middle',
-        #     dx=3  # Nudges text to right so it doesn't appear on top of the bar
-        # ).encode(
-        #     text='co2_per_capita:Q'
-        # )
+            f'year>={year_slider[0]} and year<={year_slider[1]}'),
+            title="Income Per Capita Has Been Rising"
+            ).mark_line(point=alt.OverlayMarkDef(color="blue", opacity=0.3)
+            ).encode(
+                alt.X('year', title='Year', scale=alt.Scale(domain=[year_slider[0], year_slider[1]])),
+                alt.Y('income_per_capita', title='Income Per Capita'),
+                tooltip = ["year", "income_per_capita"]).interactive()
+       
 
         return (chart).to_html()
 

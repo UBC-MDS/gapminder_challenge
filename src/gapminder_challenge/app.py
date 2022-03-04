c828c51879086d76deda8215c4929062b7ee7e3f
Julien Gordon <junigord@gmail.com>
2022-03-03 17:01:26 -0800

    minor changes and bug fixes for app3

--- src/gapminder_challenge/app.py
+++ src/gapminder_challenge/app.py
@@ -12,5 +12,5 @@ app = dash_app3.add_dash(app)
 @app.route('/')
 
 def index():
-    return render_template('index.html', app1=dash_app1.url, app2=dash_app2.url)
+    return render_template('index.html', app1=dash_app1.url, app2=dash_app2.url, app3=dash_app3.url)
 

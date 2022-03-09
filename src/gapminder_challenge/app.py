a74d3813bf8daf98b8a2c8f3a6c7828b9e0485cd
SON CHAU <soncqb@gmail.com>
2022-03-08 22:11:44 -0800

    Update app.py

--- src/gapminder_challenge/app.py
+++ src/gapminder_challenge/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
-from dashboard import dash_app1, dash_app2, dash_app3, dash_app4
+# from dashboard import dash_app1, dash_app2, dash_app3, dash_app4
 
 
 
 
 app = Flask(__name__)
-app = dash_app1.add_dash(app)
-app = dash_app2.add_dash(app)
-app = dash_app3.add_dash(app)
-app = dash_app4.add_dash(app)
+# app = dash_app1.add_dash(app)
+# app = dash_app2.add_dash(app)
+# app = dash_app3.add_dash(app)
+# app = dash_app4.add_dash(app)
 
 
 
